6c09585711a9dc4855d0c82f4e301d223e6c9241
GrahamOrmond <grahamwormond@gmail.com>
2021-06-01 00:33:11 -0400

    login page and form component updated

--- weed-leaf/src/components/AppForm.js
+++ weed-leaf/src/components/AppForm.js
@@ -1,6 +1,46 @@
 import React, { Component } from 'react';
 
-import AppCard from './AppCard';
+class AppSelect extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+    
+    render () {
+        
+        return (
+            <div class="form-input">
+                <label>{this.props.label}</label>
+                <select></select>
+            </div>
+        );
+    }
+}
+
+class AppInput extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleChange = props.handleChange.bind(this);
+    }
+    
+    render () {
+        
+        return (
+            <div class="form-input">
+                <label>{this.props.label}</label>
+                <input 
+                    name={this.props.name}
+                    onChange={this.handleChange}
+                    type={this.props.type}
+                    value={this.props.value} 
+                    placeholder={this.props.placeholder} 
+                />
+            </div>
+        );
+    }
+}
 
 class AppForm extends Component {
 
@@ -11,9 +51,19 @@ class AppForm extends Component {
     render () {
         
         return (
-            <AppCard>
-            </AppCard>
+            <div className="app-form">
+                <div className="form-header">
+                    <h4>{this.props.title}</h4>
+                </div>
+
+                <div className="form-content">
+                    {this.props.children}
+                </div>
+                <div className="form-footer">
+                   <button className="button-blue">{this.props.submitTitle}</button>
+                </div>
+            </div>
         );
     }
 }
-export default AppForm;
+export { AppForm, AppInput, AppSelect };
--- weed-leaf/src/pages/account/LoginPage.js
+++ weed-leaf/src/pages/account/LoginPage.js
@@ -1,9 +1,84 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import AppCard from '../../components/AppCard';
-import AppForm from '../../components/AppForm';
+import { AppForm, AppInput } from '../../components/AppForm';
 import AppModal from "../../components/AppModal"
 
 import './account.css'
+
+const loginExtra = () => {
+
+    return (
+        <div className="login-extra">
+            <div className="login-info">
+                <p>By continuing, you agree to our <Link to="/">User Agreement</Link> and <Link to="/">Privacy Policy</Link>.</p>
+            </div>
+            <div className="login-info">
+                <p><Link to="/account/register">Forgot your username or password?</Link></p>
+                <p><Link to="/account/register">Sign Up Here</Link></p>
+            </div>
+        </div>
+    );
+}
+
+
+
+class LoginForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            'loginData': {
+                'username': '',
+                'password': ''
+            }
+        }
+
+        this.updateInput = this.updateInput.bind(this);
+    }
+
+    updateInput(event){
+        console.log(event.target.name)
+        console.log(event.target.value)
+
+        let newState = this.state.loginData;
+        console.log(newState)
+        newState[event.target.name] = event.target.value
+        this.setState({
+            'loginData': newState
+        });
+
+        console.log(this.state)
+    }
+
+
+    render () {
+        return (
+            <AppForm 
+                title="Login"
+                submitTitle="Log In"
+            >
+                <AppInput 
+                    label="Email" 
+                    name="username"
+                    type="text" 
+                    placeholder="" 
+                    value={this.state.loginData.username}
+                    handleChange={this.updateInput}
+                />
+                <AppInput 
+                    label="Password" 
+                    name="password"
+                    type="password" 
+                    placeholder="" 
+                    value={this.state.loginData.password}
+                    handleChange={this.updateInput}
+                />
+            </AppForm>
+        );
+    }
+}
 
 class LoginPage extends Component {
 
@@ -17,17 +92,8 @@ class LoginPage extends Component {
         return (
             <div>
                <AppCard >
-                    <div className="login-info">
-                        <h4>Login</h4>
-                    </div>
-                    <AppForm />
-                    <div className="login-info">
-                        <p>Forgot your username or password?</p>
-                        <p>Sign Up Here</p>
-                    </div>
-                    <div className="login-info">
-                        <p>By continuing, you agree to our User Agreement and Privacy Policy.</p>
-                    </div>
+                    <LoginForm />
+                    { loginExtra() }
                 </AppCard> 
             </div>
         );
